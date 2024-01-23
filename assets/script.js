94da1a53a873bf85ab51f113a64244803e297d45
clatot <kat.clatot@gmail.com>
2024-01-23 19:10:54 +0100

    Ajout Filtre JS

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,87 @@
 const reponse = await fetch("http://localhost:5678/api/works")
 const works = await reponse.json()
-console.log(works);
+console.log(works)
 
 const galerie = document.querySelector(".gallery")
 
-for (let i = 0; i < works.length; i++) {
-    const workElement = document.createElement("figure")
-    
-    const workImg = document.createElement("img")
-    workImg.src = works[i].imageUrl
+function genererWorks(works) {
+    for (let i = 0; i < works.length; i++) {
+        const workElement = document.createElement("figure")
+        
+        const workImg = document.createElement("img")
+        workImg.src = works[i].imageUrl
 
-    const workCaption = document.createElement("figcaption")
-    workCaption.textContent = works[i].title
-    
-    galerie.appendChild(workElement)
-    workElement.appendChild(workImg)
-    workElement.appendChild(workCaption)
+        const workCaption = document.createElement("figcaption")
+        workCaption.textContent = works[i].title
+        
+        galerie.appendChild(workElement)
+        workElement.appendChild(workImg)
+        workElement.appendChild(workCaption)
+    }
 }
- 
+
+genererWorks(works)
+
+const boutonFiltrerTous = document.querySelector(".tous")
+const boutonFiltrerObjects = document.querySelector(".objets")
+const boutonFiltrerAppartements = document.querySelector(".appartements")
+const boutonFiltrerHotels = document.querySelector(".hotels")
+
+function boutonFiltrerUnselected() {
+    boutonFiltrerTous.classList.remove("selected")
+    boutonFiltrerObjects.classList.remove("selected")
+    boutonFiltrerAppartements.classList.remove("selected")
+    boutonFiltrerHotels.classList.remove("selected")
+}
+
+boutonFiltrerTous.addEventListener("click", function () {
+    document.querySelector(".gallery").innerHTML = ""
+    genererWorks(works)
+
+    boutonFiltrerUnselected()
+    boutonFiltrerTous.classList.add("selected");
+
+    console.log("tous")
+})
+
+
+boutonFiltrerObjects.addEventListener("click", function () {
+    const objetsFiltrees = works.filter(function (work) {
+        return work.categoryId === 1
+    })
+    document.querySelector(".gallery").innerHTML = ""
+    genererWorks(objetsFiltrees)
+
+    boutonFiltrerUnselected()
+    boutonFiltrerObjects.classList.add("selected");
+
+    console.log("objets")
+})
+
+
+boutonFiltrerAppartements.addEventListener("click", function () {
+    const appartementsFiltrees = works.filter(function (work) {
+        return work.categoryId === 2
+    })
+    document.querySelector(".gallery").innerHTML = ""
+    genererWorks(appartementsFiltrees)
+
+    boutonFiltrerUnselected()
+    boutonFiltrerAppartements.classList.add("selected");
+
+    console.log("appartements")
+})
+
+
+boutonFiltrerHotels.addEventListener("click", function () {
+    const hotelsFiltrees = works.filter(function (work) {
+        return work.categoryId === 3
+    })
+    document.querySelector(".gallery").innerHTML = ""
+    genererWorks(hotelsFiltrees)
+
+    boutonFiltrerUnselected()
+    boutonFiltrerHotels.classList.add("selected");
+
+    console.log("hotels")
+})
