66e847b4d8cf97a5e8c048fb3d8ac496917c38bf
clatot <kat.clatot@gmail.com>
2024-01-31 15:44:15 +0100

    Refonte fonction genererWorks Ajout Modal1

--- assets/script.js
+++ assets/script.js
@@ -2,21 +2,16 @@ const reponse = await fetch("http://localhost:5678/api/works")
 const works = await reponse.json()
 console.log(works)
 
-const galerie = document.querySelector(".gallery")
 
 function genererWorks(works) {
+    const galerie = document.querySelector(".gallery")
     for (let i = 0; i < works.length; i++) {
-        const workElement = document.createElement("figure")
-        
-        const workImg = document.createElement("img")
-        workImg.src = works[i].imageUrl
-
-        const workCaption = document.createElement("figcaption")
-        workCaption.textContent = works[i].title
-        
-        galerie.appendChild(workElement)
-        workElement.appendChild(workImg)
-        workElement.appendChild(workCaption)
+        galerie.innerHTML += `
+            <figure>
+                <img src="${works[i].imageUrl}"></img>
+                <figcaption>${works[i].title}</figcaption>
+            <figure>
+        `
     }
 }
 
