13da773597d49dfa8c947a8ec48f351a3a84e113
clatot <kat.clatot@gmail.com>
2024-02-19 18:27:54 +0100

    Ajout boutons filtres dynamiques

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 // Appel API GET works
-let response = await fetch("http://localhost:5678/api/works");
-let works = await response.json();
+let responseWorks = await fetch("http://localhost:5678/api/works");
+let works = await responseWorks.json();
+
+// Appel API GET works
+let responseCategories = await fetch("http://localhost:5678/api/categories");
+let categories = await responseCategories.json();
+console.log(categories)
 
 // Fonction Génération galerie 
 function genererWorks(works) {
@@ -18,6 +23,18 @@ function genererWorks(works) {
 
 // Mise en place Galerie 
 genererWorks(works);
+
+// Fonction Génération Filtres 
+function genererFilter() {
+    const filter = document.querySelector(".filter")
+    for (let i = 0; i < categories.length; i++) {
+        filter.innerHTML += `
+            <p>${categories[i].name}</p>
+        `
+    }
+}
+
+genererFilter();
 
 // Variables Filtres
 const boutonsFiltrer = document.querySelectorAll(".filter > p");
